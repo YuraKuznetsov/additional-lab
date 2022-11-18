7236886839541b01d016c71a3a6db01c5fa1cc77
Yurii Kuznetsov <yurakuznetsov23@gmail.com>
2022-11-18 11:21:48 +0100

    Initial commit

---
+++ src/main/java/com/labs/additional/AdditionalApplication.java
@@ -0,0 +1,13 @@
+package com.labs.additional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdditionalApplication.class, args);
+	}
+
+}
