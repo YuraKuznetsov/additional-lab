543bf02965f6528a1c361fba1353df460fe3ee1b
Yurii Kuznetsov <yurakuznetsov23@gmail.com>
2022-11-24 14:15:50 +0100

    Add function getUserEquation

--- src/main/java/com/labs/additional/surface/EquationGenerator.java
+++ src/main/java/com/labs/additional/surface/EquationGenerator.java
@@ -1,12 +1,16 @@
 package com.labs.additional.surface;
 
-import com.labs.additional.surface.type.SurfaceType;
 import com.labs.additional.surface.type.WideSurfaceType;
 
 import java.util.Map;
-import java.util.NoSuchElementException;
 
 public class EquationGenerator {
+    private static final Map<String, String> coefficientMeaning = Map.of(
+            "a11", "x²", "a22", "y²", "a33", "z²",
+            "a12", "xy", "a13", "xz", "a14", "x",
+            "a23", "yz", "a24", "y",
+            "a34", "z", "a44", ""
+    );
     public static String getCanonical(Map<String, Double> values, WideSurfaceType wideSurfaceType) {
         return "";
 //        double coefficient;
@@ -25,5 +29,22 @@ public class EquationGenerator {
 //            case "paraboloid cylinder":
 //                return lambda1 + "x^2 + " + 2 * Math.sqrt(-K3/I1) + "y = 0";
 //        }
+    }
+
+    public static String getUserEquation(Map<String, String> request) {
+        StringBuilder equation = new StringBuilder();
+
+        for (String key : request.keySet()) {
+            String value = request.get(key);
+            if (value.isEmpty()) continue;
+            equation.append(value).append(coefficientMeaning.get(key));
+            equation.append(" + ");
+        }
+
+        int equationLength = equation.length();
+        equation.replace(equationLength - 2, equationLength, "");
+        equation.append("= 0");
+
+        return equation.toString();
     }
 }
