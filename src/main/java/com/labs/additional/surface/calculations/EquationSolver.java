0e12a4e3ab731e73c8407ee7450455553a556cf2
Yurii Kuznetsov <yurakuznetsov23@gmail.com>
2022-11-26 17:51:27 +0100

    Optimize imports in EquationSolver

--- src/main/java/com/labs/additional/surface/calculations/EquationSolver.java
+++ src/main/java/com/labs/additional/surface/calculations/EquationSolver.java
@@ -1,9 +1,7 @@
 package com.labs.additional.surface.calculations;
 
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 public class EquationSolver {
 
