42d4a08f10de2fcaea5ed9edc6fe7cca958e5f67
Yurii Kuznetsov <yurakuznetsov23@gmail.com>
2022-11-27 18:27:06 +0100

    Correct SurfaceValidator

--- src/main/java/com/labs/additional/surface/SurfaceValidator.java
+++ src/main/java/com/labs/additional/surface/SurfaceValidator.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class SurfaceValidator {
-    private final List<String> importantCoefficientKeys = List.of("a11", "a12", "a13", "a22", "23", "a33");
+    private final List<String> importantCoefficientKeys = List.of("a11", "a12", "a13", "a22", "a23", "a33");
     private final Set<String> allCoefficientKeys = Set.of("a11", "a12", "a13", "a22", "a23", "a33",
                                                        "a14", "a24", "a34", "a44");
     private final Map<String, String> request;
