9d34ed1bd271e5b932ee51cc6be4c32cc4adf6f7
Yurii Kuznetsov <yurakuznetsov23@gmail.com>
2022-11-30 14:50:20 +0100

    Create tests to TypeFounderTest

--- src/test/java/com/labs/additional/surface/TypeFounderTest.java
+++ src/test/java/com/labs/additional/surface/TypeFounderTest.java
@@ -1,6 +1,8 @@
 package com.labs.additional.surface;
 
 import com.labs.additional.surface.type.SurfaceType;
+import com.labs.additional.surface.type.WideSurfaceType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -8,13 +10,24 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TypeFounderTest {
+    TypeFounder typeFounder;
+
+    @BeforeEach
+    void setUp() {
+        Map<String, Double> values = Map.of(
+                "I1", -13.0, "I2", 28.5, "I3", 93.5, "I4", 396.56,
+                "K2", -22.25, "K3", 251.75,
+                "lambda1", -6.87, "lambda2", -7.86, "lambda3", 1.73);
+        typeFounder = new TypeFounder(values);
+    }
 
     @Test
-    void plane_whenAllValuesAreZero() {
-        Map<String, Double> values = Map.of(
-                "I1", 0d, "I2", 0d, "I3", 0d, "I4", 0d,
-                "K2", 0d, "K3", 0d,
-                "lambda1", 0d, "lambda2", 0d, "lambda3", 0d);
-        assertEquals(SurfaceType.PLANE, new TypeFounder(values).getType());
+    void testGetWideType() {
+        assertEquals(WideSurfaceType.FULL_SQUARE, typeFounder.getWideType());
+    }
+
+    @Test
+    void testGetType() {
+        assertEquals(SurfaceType.ONE_LEAF_HYPERBOLOID, typeFounder.getType());
     }
 }
