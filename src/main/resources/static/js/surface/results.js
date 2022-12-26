4a791ed420caf96ef1caa774764fe083d59f4327
Yurii Kuznetsov <yurakuznetsov23@gmail.com>
2022-12-26 15:20:51 +0100

    Refactor results.js again

--- src/main/resources/static/js/surface/results.js
+++ src/main/resources/static/js/surface/results.js
@@ -1,42 +1,39 @@
+class SurfaceMenu {
+    constructor(surface) {
+        this.header = surface.querySelector(".surface__header");
+        this.icon = surface.querySelector(".surface__icon");
+        this.body = surface.querySelector(".surface__body");
+    }
+
+    showBody() {
+        this.header.classList.add("fill-bg");
+        this.body.classList.add("shown");
+        this.icon.classList.add("turn-over");
+    }
+
+    hideBody() {
+        this.header.classList.remove("fill-bg");
+        this.body.classList.remove("shown");
+        this.icon.classList.remove("turn-over");
+    }
+
+    isBodyShown() {
+        return this.body.classList.contains("shown");
+    }
+
+    activate() {
+        this.header.addEventListener("click", () => {
+            if (this.isBodyShown()) {
+                this.hideBody();
+            } else {
+                this.showBody();
+            }
+        });
+    }
+}
+
 const surfaces = document.querySelectorAll(".surface");
 
-function showSurface(surface) {
-    const header = surface.querySelector(".surface__header");
-    const icon = surface.querySelector(".surface__icon");
-    const body = surface.querySelector(".surface__body");
-
-    header.classList.add("fill-bg");
-    body.classList.add("shown");
-    icon.classList.add("turn-over");
-}
-
-function closeSurface(surface) {
-    const header = surface.querySelector(".surface__header");
-    const icon = surface.querySelector(".surface__icon");
-    const body = surface.querySelector(".surface__body");
-
-    header.classList.remove("fill-bg");
-    body.classList.remove("shown");
-    icon.classList.remove("turn-over");
-}
-
-function isSurfaceBodyShown(surface) {
-    const surfaceBody = surface.querySelector(".surface__body");
-    return surfaceBody.classList.contains("shown");
-}
-
-function addListener(surface) {
-    const surfaceHeader = surface.querySelector(".surface__header");
-
-    surfaceHeader.addEventListener("click", () => {
-        if (isSurfaceBodyShown(surface)) {
-            closeSurface(surface);
-        } else {
-            showSurface(surface)
-        }
-    });
-}
-
 for (let surface of surfaces) {
-    addListener(surface);
+    new SurfaceMenu(surface).activate();
 }
