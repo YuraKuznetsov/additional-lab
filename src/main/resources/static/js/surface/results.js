e9b31234b13545f66bc1a6d221cd698ab63ed44e
Yurii Kuznetsov <yurakuznetsov23@gmail.com>
2022-12-26 14:56:46 +0100

    Refactor results.js

--- src/main/resources/static/js/surface/results.js
+++ src/main/resources/static/js/surface/results.js
@@ -1,31 +1,42 @@
 const surfaces = document.querySelectorAll(".surface");
 
-for (let surface of surfaces) {
+function showSurface(surface) {
     const header = surface.querySelector(".surface__header");
     const icon = surface.querySelector(".surface__icon");
     const body = surface.querySelector(".surface__body");
 
-    header.addEventListener("click", e => {
-        if (body.classList.contains("shown")) {
-            header.classList.remove("fill-bg");
-            body.classList.remove("shown");
-            icon.classList.remove("turn-over");
+    header.classList.add("fill-bg");
+    body.classList.add("shown");
+    icon.classList.add("turn-over");
+}
+
+function closeSurface(surface) {
+    const header = surface.querySelector(".surface__header");
+    const icon = surface.querySelector(".surface__icon");
+    const body = surface.querySelector(".surface__body");
+
+    header.classList.remove("fill-bg");
+    body.classList.remove("shown");
+    icon.classList.remove("turn-over");
+}
+
+function isSurfaceBodyShown(surface) {
+    const surfaceBody = surface.querySelector(".surface__body");
+    return surfaceBody.classList.contains("shown");
+}
+
+function addListener(surface) {
+    const surfaceHeader = surface.querySelector(".surface__header");
+
+    surfaceHeader.addEventListener("click", () => {
+        if (isSurfaceBodyShown(surface)) {
+            closeSurface(surface);
         } else {
-            header.classList.add("fill-bg");
-            body.classList.add("shown");
-            icon.classList.add("turn-over");
+            showSurface(surface)
         }
-        
     });
 }
 
-function showBody(body) {
-    body.style.opacity = "1";
-    body.style.height = "auto";
-}
-
-function hideBody(body) {
-    body.style.opacity = "0";
-    body.style.height = "0";
-    body.style.overflow = "hidden";
+for (let surface of surfaces) {
+    addListener(surface);
 }
