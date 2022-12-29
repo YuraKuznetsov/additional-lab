db1c2c7b8af043b40a54090dd6183143a4f7000a
Yurii Kuznetsov <yurakuznetsov23@gmail.com>
2022-12-29 12:28:53 +0100

    Add fields to Surface model

--- src/main/java/com/labs/additional/model/Surface.java
+++ src/main/java/com/labs/additional/model/Surface.java
@@ -1,13 +1,30 @@
 package com.labs.additional.model;
 
+import com.labs.additional.service.surface.type.SurfaceType;
+
+import java.util.Map;
+
 public class Surface {
     private final String userEquation;
+    private final Map<String, Double> values;  // I and K
+    private final String cubicEquation;
+    private final String canonicalEquationExplain;
+    private final String canonicalEquationFormula;
+    private final String simpleCanonicalEquation;
     private final String canonicalEquation;
-    private final String surfaceType;
+    private final SurfaceType surfaceType;
     private final String imgSrc;
 
-    public Surface(String userEquation, String canonicalEquation, String surfaceType, String imgSrc) {
+    public Surface(String userEquation, Map<String, Double> values, String cubicEquation,
+                   String canonicalEquationExplain, String canonicalEquationFormula, String simpleCanonicalEquation,
+                   String canonicalEquation, SurfaceType surfaceType, String imgSrc) {
+
         this.userEquation = userEquation;
+        this.values = values;
+        this.cubicEquation = cubicEquation;
+        this.canonicalEquationExplain = canonicalEquationExplain;
+        this.canonicalEquationFormula = canonicalEquationFormula;
+        this.simpleCanonicalEquation = simpleCanonicalEquation;
         this.canonicalEquation = canonicalEquation;
         this.surfaceType = surfaceType;
         this.imgSrc = imgSrc;
@@ -17,11 +34,31 @@ public class Surface {
         return userEquation;
     }
 
+    public Map<String, Double> getValues() {
+        return values;
+    }
+
+    public String getCubicEquation() {
+        return cubicEquation;
+    }
+
+    public String getCanonicalEquationExplain() {
+        return canonicalEquationExplain;
+    }
+
+    public String getCanonicalEquationFormula() {
+        return canonicalEquationFormula;
+    }
+
+    public String getSimpleCanonicalEquation() {
+        return simpleCanonicalEquation;
+    }
+
     public String getCanonicalEquation() {
         return canonicalEquation;
     }
 
-    public String getSurfaceType() {
+    public SurfaceType getSurfaceType() {
         return surfaceType;
     }
 
