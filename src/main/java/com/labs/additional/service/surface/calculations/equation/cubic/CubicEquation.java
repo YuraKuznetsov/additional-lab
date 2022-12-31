011633c4fdf96422c6b4b747f839a36f649d6650
Yurii Kuznetsov <yurakuznetsov23@gmail.com>
2022-12-31 22:26:49 +0100

    Changes in CubicEquation

---
+++ src/main/java/com/labs/additional/service/surface/calculations/equation/cubic/CubicEquation.java
@@ -0,0 +1,79 @@
+package com.labs.additional.service.surface.calculations.equation.cubic;
+
+import com.labs.additional.excepiton.IsNotCubicEquationException;
+
+public class CubicEquation {
+    private double d, a, b, c;
+    private double p, q, D;
+    private CubicRoots cubicRoots;
+
+    public CubicRoots calculateCubicRoots(double d, double a, double b, double c) {
+        if (d == 0) throw new IsNotCubicEquationException("Parameter d must not be zero");
+        this.d = d;
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        divideEquationCoefficients();
+        calculateP();
+        calculateQ();
+        calculateDeterminant();
+        findCubicRoots();
+
+        return cubicRoots;
+    }
+
+    private void divideEquationCoefficients() {
+        a /= d;
+        b /= d;
+        c /= d;
+    }
+
+    private void calculateP() {
+        p = b - (a*a) / 3;
+    }
+
+    private void calculateQ() {
+        q = (2 * (a*a*a) / 27) - (a * b / 3) + c;
+    }
+
+    private void calculateDeterminant() {
+        D = (p*p*p / 27) + (q*q / 4);
+    }
+
+    private void findCubicRoots() {
+        if (round3(D) > 0) {
+            findRootWithPositiveDeterminant();
+        } else if (round3(D) == 0d) {
+            findRootsWithZeroDeterminant();
+        } else {
+            findRootsWithNegativeDeterminant();
+        }
+    }
+
+    private double round3(double value) {
+        value = Math.round(value * 1000);
+        return value / 1000;
+    }
+
+    private void findRootWithPositiveDeterminant() {
+        double root1 = Math.cbrt(-q / 2 + Math.sqrt(D)) + Math.cbrt(-q / 2 - Math.sqrt(D)) - a / 3;
+
+        cubicRoots = new CubicRoots(root1);
+    }
+
+    private void findRootsWithZeroDeterminant() {
+        double root1 = -2 * Math.cbrt(q / 2) - a / 3;
+        double root2 = Math.cbrt(q / 2) - a / 3;
+
+        cubicRoots = new CubicRoots(root1, root2, root2);
+    }
+
+    private void findRootsWithNegativeDeterminant() {
+        double root1 = (2.0 / Math.sqrt(3)) * (Math.sqrt(-p) * Math.sin((1 / 3.0) * Math.asin(((3 * Math.sqrt(3) * q) / (2 * Math.pow(Math.pow(-p, (double) 1 / 2), 3)))))) - (a / 3.0);
+        double root2 = (-2.0 / Math.sqrt(3)) * (Math.sqrt(-p) * Math.sin((1 / 3.0) * Math.asin(((3 * Math.sqrt(3) * q) / (2 * Math.pow(Math.pow(-p, (double) 1 / 2), 3)))) + (Math.PI / 3))) - (a / 3.0);
+        double root3 = (2.0 / Math.sqrt(3)) * (Math.sqrt(-p) * Math.cos((1 / 3.0) * Math.asin(((3 * Math.sqrt(3) * q) / (2 * Math.pow(Math.pow(-p, (double) 1 / 2), 3)))) + (Math.PI / 6))) - (a / 3.0);
+
+        cubicRoots = new CubicRoots(root1, root2, root3);
+    }
+}
--- src/test/java/com/labs/additional/service/surface/calculations/CubicEquationTest.java
+++ src/test/java/com/labs/additional/service/surface/calculations/CubicEquationTest.java
@@ -1,10 +1,10 @@
 package com.labs.additional.service.surface.calculations;
 
-
+import com.labs.additional.excepiton.IsNotCubicEquationException;
+import com.labs.additional.service.surface.calculations.equation.cubic.CubicEquation;
+import com.labs.additional.service.surface.calculations.equation.cubic.CubicRoots;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,37 +18,48 @@ class CubicEquationTest {
     }
 
     @Test
-    void isEmpty_whenAllCubicCoefficientsAreZero() {
-        List<Double> expected = List.of();
-        List<Double> result = cubicEquation.solve(1.0, 0.0, 0.0, 0.0);
-        assertEquals(expected, result);
+    void throwException_whenDIsZero() {
+        assertThrows(IsNotCubicEquationException.class,
+                     () -> cubicEquation.calculateCubicRoots(0, 10, -2, 14.3));
     }
 
     @Test
-    void oneCubicNotZeroRoot_whenGivenOnlyNotZeroI1() {
+    void isEmpty_whenAllCubicCoefficientsAreZero() {
+        CubicRoots cubicRoots = cubicEquation.calculateCubicRoots(1.0, 0.0, 0.0, 0.0);
+
+        assertTrue(cubicRoots.getRoot1().isPresent());
+        assertTrue(cubicRoots.getRoot2().isPresent());
+        assertTrue(cubicRoots.getRoot3().isPresent());
+
+        assertEquals(-0.0, cubicRoots.getRoot1().get());
+        assertEquals(0, cubicRoots.getRoot2().get());
+        assertEquals(0, cubicRoots.getRoot3().get());
+    }
+
+    @Test
+    void twoZeroCubicRoot_whenGivenOnlyI1() {
         for (double I1 = -10; I1 <= 10; I1 += .5) {
             if (I1 == 0) continue;
-            List<Double> expected = List.of(-I1);
-            List<Double> result = cubicEquation.solve(1.0, I1, 0.0, 0.0);
 
-            assertEquals(expected, result);
+            CubicRoots cubicRoots = cubicEquation.calculateCubicRoots(1.0, I1, 0.0, 0.0);
+
+            assertTrue(cubicRoots.getRoot1().isPresent());
+            assertTrue(cubicRoots.getRoot2().isPresent());
+            assertTrue(cubicRoots.getRoot3().isPresent());
+
+            assertEquals(-I1, cubicRoots.getRoot1().get());
+            assertEquals(0, cubicRoots.getRoot2().get());
+            assertEquals(0, cubicRoots.getRoot3().get());
         }
     }
 
     @Test
-    void threeCubicNotZeroRoots_whenNotZeroI3() {
+    void threeCubicRoots_whenGivenI3() {
         final double I3 = 5.1;
-        final int expectedSize = 3;
-        List<Double> resul = cubicEquation.solve(1.0, 13.4, -22.5, I3);
-        assertEquals(expectedSize, resul.size());
-    }
+        CubicRoots cubicRoots = cubicEquation.calculateCubicRoots(1.0, 13.4, -22.5, I3);
 
-    @Test
-    void lessThanThreeCubicRoots_whenZeroI3() {
-        final double I3 = 0;
-        List<Double> resul = cubicEquation.solve(1.0, 13.4, -22.5, I3);
-        System.out.println(resul);
-        assertTrue(resul.size() < 3);
+        assertTrue(cubicRoots.getRoot1().isPresent());
+        assertTrue(cubicRoots.getRoot2().isPresent());
+        assertTrue(cubicRoots.getRoot3().isPresent());
     }
-
 }
