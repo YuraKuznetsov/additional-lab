3f0a45adfe9ff9babd30795b4de04af4568b2129
Yurii Kuznetsov <yurakuznetsov23@gmail.com>
2022-12-31 22:36:40 +0100

    Rename SurfaceValues

---
+++ src/main/java/com/labs/additional/service/surface/calculations/SurfaceValues.java
@@ -0,0 +1,46 @@
+package com.labs.additional.service.surface.calculations;
+
+import com.labs.additional.service.surface.calculations.equation.cubic.CubicRoots;
+
+public class SurfaceValues {
+    private final double I1, I2, I3, I4, K2, K3;
+    private final CubicRoots lambdas;
+
+    public SurfaceValues(double I1, double I2, double I3, double I4, double K2, double K3, CubicRoots lambdas) {
+        this.I1 = I1;
+        this.I2 = I2;
+        this.I3 = I3;
+        this.I4 = I4;
+        this.K2 = K2;
+        this.K3 = K3;
+        this.lambdas = lambdas;
+    }
+
+    public double getI1() {
+        return I1;
+    }
+
+    public double getI2() {
+        return I2;
+    }
+
+    public double getI3() {
+        return I3;
+    }
+
+    public double getI4() {
+        return I4;
+    }
+
+    public double getK2() {
+        return K2;
+    }
+
+    public double getK3() {
+        return K3;
+    }
+
+    public CubicRoots getLambdas() {
+        return lambdas;
+    }
+}
