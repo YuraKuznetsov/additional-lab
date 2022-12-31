33bc5ce85f6ccbe7822c031af06ebd8e8e9368d5
Yurii Kuznetsov <yurakuznetsov23@gmail.com>
2022-12-31 17:05:53 +0100

    Add MinorIsNotExistException

---
+++ src/main/java/com/labs/additional/excepiton/MinorIsNotExistException.java
@@ -0,0 +1,7 @@
+package com.labs.additional.excepiton;
+
+public class MinorIsNotExistException extends RuntimeException {
+    public MinorIsNotExistException(String massage) {
+        super(massage);
+    }
+}
