0292b3f9b30992a6a2424ae5e060f3cfde64deab
Yurii Kuznetsov <yurakuznetsov23@gmail.com>
2022-12-31 22:36:18 +0100

    Rename SurfaceCalculator

---
+++ src/main/java/com/labs/additional/service/surface/calculations/SurfaceCalculator.java
@@ -0,0 +1,59 @@
+package com.labs.additional.service.surface.calculations;
+
+import com.labs.additional.service.surface.calculations.equation.cubic.CubicEquation;
+import com.labs.additional.service.surface.calculations.equation.cubic.CubicRoots;
+
+public class SurfaceCalculator {
+    private CoefficientsA coefficients;
+    private double I1, I2, I3, I4, K2, K3;
+    private CubicRoots lambdas;
+
+    public SurfaceValues calculateSurfaceValues(CoefficientsA coefficientsA) {
+        coefficients = coefficientsA;
+        calculateI();
+        calculateK();
+        calculateLambdas();
+
+        return new SurfaceValues(I1, I2, I3, I4, K2, K3, lambdas);
+    }
+
+
+    private void calculateI() {
+        Matrix matrix4by4 = getMatrix4by4();
+        Matrix matrix3by3 = matrix4by4.getMinor(3, 3);
+
+        I1 = coefficients.getA11() + coefficients.getA22() + coefficients.getA33();
+        I2 = matrix3by3.getMinor(0, 0).getDeterminant()
+                + matrix3by3.getMinor(1, 1).getDeterminant()
+                + matrix3by3.getMinor(2, 2).getDeterminant();
+        I3 = matrix3by3.getDeterminant();
+        I4 = matrix4by4.getDeterminant();
+    }
+
+    private void calculateK() {
+        Matrix matrix4by4 = getMatrix4by4();
+
+        K2 = matrix4by4.getMinor(1, 1).getMinor(0, 0).getDeterminant()
+                +  matrix4by4.getMinor(2, 2).getMinor(0, 0).getDeterminant()
+                +  matrix4by4.getMinor(2, 2).getMinor(1, 1).getDeterminant();
+
+        K3 = matrix4by4.getMinor(0, 0).getDeterminant()
+                + matrix4by4.getMinor(1, 1).getDeterminant()
+                + matrix4by4.getMinor(2, 2).getDeterminant();
+    }
+
+    private void calculateLambdas() {
+        CubicEquation cubicEquation = new CubicEquation();
+        lambdas = cubicEquation.calculateCubicRoots(1, -I1, I2, -I3);
+    }
+
+    private Matrix getMatrix4by4() {
+        double[][] matrixAsArray = new double[][] {
+                {coefficients.getA11(), coefficients.getA12(), coefficients.getA13(), coefficients.getA14()},
+                {coefficients.getA12(), coefficients.getA22(), coefficients.getA23(), coefficients.getA24()},
+                {coefficients.getA13(), coefficients.getA23(), coefficients.getA33(), coefficients.getA34()},
+                {coefficients.getA14(), coefficients.getA24(), coefficients.getA34(), coefficients.getA44()}};
+
+        return new Matrix(matrixAsArray);
+    }
+}
---
+++ src/test/java/com/labs/additional/service/surface/calculations/SurfaceCalculatorTest.java
@@ -0,0 +1,74 @@
+package com.labs.additional.service.surface.calculations;
+
+import com.labs.additional.service.surface.calculations.equation.cubic.CubicRoots;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SurfaceCalculatorTest {
+    SurfaceCalculator surfaceCalculator;
+
+    @Test
+    void zeroValues_whenAllCoefficientAreZero() {
+        CoefficientsA zeroCoefficients = new CoefficientsA(
+                0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0
+        );
+
+        surfaceCalculator = new SurfaceCalculator();
+        SurfaceValues surfaceValues = surfaceCalculator.calculateSurfaceValues(zeroCoefficients);
+
+        assertEquals(0, surfaceValues.getI1());
+        assertEquals(0, surfaceValues.getI2());
+        assertEquals(0, surfaceValues.getI3());
+        assertEquals(0, surfaceValues.getI4());
+        assertEquals(0, surfaceValues.getK2());
+        assertEquals(0, surfaceValues.getK3());
+
+        CubicRoots lambdas = surfaceValues.getLambdas();
+        Optional<Double> lambda1 = lambdas.getRoot1();
+        Optional<Double> lambda2 = lambdas.getRoot2();
+        Optional<Double> lambda3 = lambdas.getRoot3();
+
+        assertTrue(lambda1.isPresent());
+        assertTrue(lambda2.isPresent());
+        assertTrue(lambda3.isPresent());
+
+        assertEquals(0, lambda1.get());
+        assertEquals(0, lambda2.get());
+        assertEquals(0, lambda3.get());
+    }
+
+    @Test
+    void testCalcValues() {
+        CoefficientsA coefficientsA = new CoefficientsA(
+                3d, 0d, 0d, -1d, -2d,
+                2.5, 0d, 0d, 0d, 0d
+        );
+        surfaceCalculator = new SurfaceCalculator();
+        SurfaceValues surfaceValues = surfaceCalculator.calculateSurfaceValues(coefficientsA);
+
+        assertEquals(1, surfaceValues.getI1());
+        assertEquals(-12.25, surfaceValues.getI2());
+        assertEquals(-18.75, surfaceValues.getI3());
+        assertEquals(6.25, surfaceValues.getI4());
+        assertEquals(-1, surfaceValues.getK2());
+        assertEquals(2, surfaceValues.getK3());
+
+        CubicRoots lambdas = surfaceValues.getLambdas();
+        Optional<Double> lambda1 = lambdas.getRoot1();
+        Optional<Double> lambda2 = lambdas.getRoot2();
+        Optional<Double> lambda3 = lambdas.getRoot3();
+
+        assertTrue(lambda1.isPresent());
+        assertTrue(lambda2.isPresent());
+        assertTrue(lambda3.isPresent());
+
+        assertEquals(1.69, lambda1.get());
+        assertEquals(-3.69, lambda2.get());
+        assertEquals(3, lambda3.get());
+
+    }
+}
