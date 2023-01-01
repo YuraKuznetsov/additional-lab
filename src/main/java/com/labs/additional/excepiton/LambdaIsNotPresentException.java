c38c8f7c93f91b44cd0dc5b97dfc0a0ced38547e
Yurii Kuznetsov <yurakuznetsov23@gmail.com>
2023-01-01 12:08:12 +0100

    Create LambdaIsNotPresentException

---
+++ src/main/java/com/labs/additional/excepiton/LambdaIsNotPresentException.java
@@ -0,0 +1,7 @@
+package com.labs.additional.excepiton;
+
+public class LambdaIsNotPresentException extends RuntimeException {
+    public LambdaIsNotPresentException(String massage) {
+        super(massage);
+    }
+}
